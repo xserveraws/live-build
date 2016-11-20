d0b6eb26dcb0fa860a7a8543dd1ccff13749163c
Chris Lamb <lamby@debian.org>
2016-11-20 16:27:30 +0100

    Factor out clamping mtimes into its own method.
    Signed-off-by: Chris Lamb <lamby@debian.org>

---
+++ functions/reproducible.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## live-build(7) - System Build Scripts
+## Copyright (C) 2016 Chris Lamb <lamby@debian.org>
+##
+## live-build comes with ABSOLUTELY NO WARRANTY; for details see COPYING.
+## This is free software, and you are welcome to redistribute it
+## under certain conditions; see COPYING for details.
+
+
+# "Clamp" the time to SOURCE_DATE_EPOCH when the file is more recent to keep
+# the original times for files that have not been created or modified during
+# the build process:
+Clamp_mtimes ()
+{
+	find "${@}" -newermt "@${SOURCE_DATE_EPOCH}" -print0 | \
+		xargs -0r touch --no-dereference --date="@${SOURCE_DATE_EPOCH}"
+}
